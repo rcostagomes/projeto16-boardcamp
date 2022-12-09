d9230df5ce8b5322a292e0792b44f8bcf620d45a
RodrigoCosta <costardgg@gmail.com>
2022-12-08 21:12:45 -0300

    feat: Post /categories

--- src/server.js
+++ src/server.js
@@ -16,9 +16,40 @@ const connection = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+const nameSchema = joi.object({
+  name: joi.string().min(1),
+});
+
 app.get("/categories", async (req, res) => {
   const categorias = await connection.query(`SELECT * FROM categories`);
   res.send(categorias.rows);
+});
+
+app.post("/categories", async (req, res) => {
+  const { name } = req.body;
+  const newName = {
+    name,
+  };
+  console.log(name);
+  const validation = nameSchema.validate(newName, { abortEarly: false });
+  if (validation.error) {
+    const error = validation.error.details.map((d) => d.message);
+    return res.status(422).send(error);
+  }
+  try {
+    const nameExist = await connection.query(
+      "SELECT * FROM categories WHERE name = $1;",
+      [name]
+    );
+    if (nameExist.rows[0]) {
+      return res.status(409).send("Categoria já existente");
+    }
+    await connection.query(`INSERT INTO categories (name)VALUES ($1)`, [name]);
+
+    res.status(201).send(`Categoria ${name} Criada`);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(port, () => console.log(`app runing in port ${port}`));
