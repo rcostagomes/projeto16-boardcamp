d5a68063a72a61e9ff1ecf2c84718068e94ed421
RodrigoCosta <costardgg@gmail.com>
2022-12-10 10:43:32 -0300

    feat:Post and Get /games

--- src/server.js
+++ src/server.js
@@ -20,18 +20,24 @@ const nameSchema = joi.object({
   name: joi.string().min(1),
 });
 
+const gameSchema = joi.object({
+  name: joi.string().min(1),
+  image: joi.string(),
+  stockTotal: joi.number().integer().min(1),
+  categoryId: joi.number().integer().min(1),
+  pricePerDay: joi.number(),
+});
 app.get("/categories", async (req, res) => {
-  const categorias = await connection.query(`SELECT * FROM categories`);
-  res.send(categorias.rows);
+  const categories = await connection.query(`SELECT * FROM categories`);
+  res.send(categories.rows);
 });
 
 app.post("/categories", async (req, res) => {
   const { name } = req.body;
-  const newName = {
+  const newCategory = {
     name,
   };
-  console.log(name);
-  const validation = nameSchema.validate(newName, { abortEarly: false });
+  const validation = nameSchema.validate(newCategory, { abortEarly: false });
   if (validation.error) {
     const error = validation.error.details.map((d) => d.message);
     return res.status(422).send(error);
@@ -50,6 +56,54 @@ app.post("/categories", async (req, res) => {
   } catch (err) {
     console.log(err);
   }
+});
+
+app.get("/games", async (req, res) => {
+  const games = await connection.query(`SELECT * FROM games`);
+  res.send(games.rows);
+});
+
+app.post("/games", async (req, res) => {
+  const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+  const newGame = {
+    name,
+    image,
+    stockTotal,
+    categoryId,
+    pricePerDay,
+  };
+  
+  const validation = gameSchema.validate(newGame, { abortEarly: false });
+  if (validation.error) {
+    const error = validation.error.details.map((d) => d.message);
+    return res.status(422).send(error);
+  }
+  try {
+    const idExist = await connection.query(
+      `SELECT * FROM categories WHERE id =$1`,
+      [categoryId]
+    );
+    if (!idExist.rows[0]) {
+      return res.status(400).send("Categoria não existente");
+    }
+    const gameExist = await connection.query(
+      `SELECT * FROM games WHERE name = $1`,
+      [name]
+    );
+
+    if (gameExist.rows[0]) {
+      return res.status(409).send("Jogo já cadastrado");
+    }
+
+    await connection.query(
+      `INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5)`,
+      [name, image, stockTotal, categoryId, pricePerDay]
+    );
+  
+  } catch (err) {
+    console.log(err);
+  }
+  res.status(201).send(`${name} cadastrado com sucesso`);
 });
 
 app.listen(port, () => console.log(`app runing in port ${port}`));
